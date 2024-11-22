f1d43dadc2ab32e133379d613a4a212b5b149f97
Franz Haas <franz.dominik.haas@gmail.com>
2024-11-22 18:11:01 +0100

    - build doc

--- src/robotframework_construct/regmap.py
+++ src/robotframework_construct/regmap.py
@@ -34,12 +34,15 @@ class regmap(_construct_interface_basics):
                 assert False, f"could not find register {reg} in regmap {identifier}, register out of bound"
         return reg,relevantStruct
 
-    @keyword('Remove register map `${identifier}´')
-    def remove_register_map(self, identifier: str):
-        del self._regmaps[identifier]
-
     @keyword('Register regmap `${spec}´ from `${library}´ for `${identifier}´')
     def register_regmap(self, spec: str, library: str, identifier: str):
+        """Makes a regmap available with name identifier
+
+        Arguments:
+        - spec: the name of the construct to be used (variable name of the Struct inside the library)
+        - library: the name of module where the construct is located (what you would import)
+        - identifier: the name of the regmap to be used afterwards
+        """
         lib = importlib.import_module(library)
         spec = getattr(lib, spec)
         assert isinstance(spec, construct.Construct), f"spec should be a Construct, but was {type(spec)}"
@@ -50,9 +53,29 @@ class regmap(_construct_interface_basics):
         assert len(set(item.sizeof() for item in spec.subcons)) in {1}, "All elements of the construct regmap need to have the same size"
 
         self._regmaps[identifier].regmap = spec
+    
+    @keyword('Remove register map `${identifier}´')
+    def remove_register_map(self, identifier: str):
+        """Removes a previously registered regmap
+
+        Arguments:
+        - identifier: the name of the regmap to be removed
+        """
+        del self._regmaps[identifier]
 
     @keyword('Register read register access function `${spec}´ from `${library}´ for `${identifier}´')
     def register_read_register_access_function(self, spec: str, library: str, identifier: str):
+        """Register a read function for a previously registered regmap
+
+        This function needs to accept 1 argument, and have one return value:
+            - addr: the Address of the Register to be read
+            - return: the raw binary value of the register as bytes
+
+        Arguments:
+        - spec: the name of the function to be used (name of the function inside the library)
+        - library: the name of module where the function is located (what you would import)
+        - identifier: the name of the previously registered regmap to be used
+        """
         lib = importlib.import_module(library)
         spec = getattr(lib, spec)
         assert callable(spec), f"spec should be a callable, but was {type(spec)}"
@@ -61,6 +84,17 @@ class regmap(_construct_interface_basics):
 
     @keyword('Register write register access function `${spec}´ from `${library}´ for `${identifier}´')
     def register_write_register_access_function(self, spec: str, library: str, identifier: str):
+        """Register a write function for a previously registered regmap
+
+        This function needs to accept 2 arguments, the return value is ignored:
+            - addr: the Address of the Register to be read
+            - value: the raw binary value to be written as bytes
+
+        Arguments:
+        - spec: the name of the function to be used (name of the function inside the library)
+        - library: the name of module where the function is located (what you would import)
+        - identifier: the name of the previously registered regmap to be used
+        """
         lib = importlib.import_module(library)
         spec = getattr(lib, spec)
         assert callable(spec), f"spec should be a callable, but was {type(spec)}"
@@ -69,13 +103,26 @@ class regmap(_construct_interface_basics):
 
     @keyword('Read register `${reg}` from `${identifier}´')
     def read_register(self, reg, identifier: str):
+        """Reads a register using the registered read function of a registered regmap
+
+        Arguments:
+        - reg: the identification of the register to be read, needs to be what is expected by the read function
+        - identifier: the name of the regmap to be used
+        """
         reg, relevantStruct = self._get_subcon(reg, identifier)
         regVal = self._regmaps[identifier].read_reg(reg)
         assert isinstance(regVal, bytes), f"read register should return bytes, but returned {type(regVal)}"
         return relevantStruct.parse(regVal)
 
     @keyword('Write register `${reg}` in `${identifier}´ with `${data}´')
-    def write_register(self, reg, identifier: str, data):
+    def write_register(self, reg, identifier: str, data: [bytes, dict, construct.Struct]):
+        """Writes a register using the registered write function of a registered regmap
+
+        Arguments:
+        - reg: the identification of the register to be read, needs to be what is expected by the read function
+        - identifier: the name of the regmap to be used
+        - data: the data to be written, either as bytes or as a dictionary/Struct that can be built by the relevant struct
+        """
         reg, relevantStruct = self._get_subcon(reg, identifier)
         if isinstance(data, bytes):
             dataOut = data
